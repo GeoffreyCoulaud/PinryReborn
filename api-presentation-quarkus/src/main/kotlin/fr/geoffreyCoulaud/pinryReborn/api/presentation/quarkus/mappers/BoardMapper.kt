0bdd6081c72d0965db7a12a78f7ca4e786f35e12
GeoffreyCoulaud <geoffrey.coulaud@gmail.com>
2026-07-20 19:27:31 +0200

    feat(presentation): BoardRecycleBinController

--- api-presentation-quarkus/src/main/kotlin/fr/geoffreyCoulaud/pinryReborn/api/presentation/quarkus/mappers/BoardMapper.kt
+++ api-presentation-quarkus/src/main/kotlin/fr/geoffreyCoulaud/pinryReborn/api/presentation/quarkus/mappers/BoardMapper.kt
@@ -3,10 +3,13 @@ package fr.geoffreyCoulaud.pinryReborn.api.presentation.quarkus.mappers
 import fr.geoffreyCoulaud.pinryReborn.api.domain.entities.Board
 import fr.geoffreyCoulaud.pinryReborn.api.presentation.quarkus.dtos.output.BoardOutputDto
 import fr.geoffreyCoulaud.pinryReborn.api.presentation.quarkus.dtos.output.BoardRefDto
+import fr.geoffreyCoulaud.pinryReborn.api.presentation.quarkus.dtos.output.RecycledBoardDto
 
 object BoardMapper {
     fun Board.toRefDto() = BoardRefDto(id = id, name = name)
 
     fun Board.toDto(pinCount: Int) =
         BoardOutputDto(id = id, name = name, description = description, pinCount = pinCount)
+
+    fun Board.toRecycledDto() = RecycledBoardDto(id = id, name = name, description = description)
 }
