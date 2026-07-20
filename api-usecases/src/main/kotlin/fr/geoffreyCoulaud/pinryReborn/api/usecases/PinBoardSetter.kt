4d48b7883b7a23886e827ede0bd85991bd17d2a5
GeoffreyCoulaud <geoffrey.coulaud@gmail.com>
2026-07-20 18:50:53 +0200

    feat(usecase): PinBoardSetter (set-based pin board membership)

---
+++ api-usecases/src/main/kotlin/fr/geoffreyCoulaud/pinryReborn/api/usecases/PinBoardSetter.kt
@@ -0,0 +1,34 @@
+package fr.geoffreyCoulaud.pinryReborn.api.usecases
+
+import fr.geoffreyCoulaud.pinryReborn.api.domain.entities.Board
+import fr.geoffreyCoulaud.pinryReborn.api.domain.entities.Pin
+import fr.geoffreyCoulaud.pinryReborn.api.domain.entities.User
+import fr.geoffreyCoulaud.pinryReborn.api.domain.repositories.BoardRepositoryInterface
+import fr.geoffreyCoulaud.pinryReborn.api.domain.repositories.PinRepositoryInterface
+import fr.geoffreyCoulaud.pinryReborn.api.usecases.exceptions.PinBoardSettingInvalidBoardError
+import fr.geoffreyCoulaud.pinryReborn.api.usecases.exceptions.PinBoardSettingPermissionError
+import fr.geoffreyCoulaud.pinryReborn.api.usecases.exceptions.PinBoardSettingPinDoesNotExistError
+import fr.geoffreyCoulaud.pinryReborn.api.usecases.exceptions.PinBoardSettingSoftDeletedPinError
+import jakarta.enterprise.context.ApplicationScoped
+import java.util.UUID
+
+@ApplicationScoped
+class PinBoardSetter(
+    private val pinRepository: PinRepositoryInterface,
+    private val boardRepository: BoardRepositoryInterface,
+) {
+    fun setBoards(pinId: UUID, boardIds: List<UUID>, user: User): Pin {
+        val pin = pinRepository.findPinById(id = pinId) ?: throw PinBoardSettingPinDoesNotExistError()
+        if (pin.author != user) throw PinBoardSettingPermissionError()
+        if (pin.softDeletedAt != null) throw PinBoardSettingSoftDeletedPinError()
+
+        val boards = boardIds.map { resolveBoard(it, user) }
+        return pinRepository.savePin(pin.copy(boards = boards))
+    }
+
+    private fun resolveBoard(boardId: UUID, user: User): Board {
+        val board = boardRepository.findActiveBoardById(boardId) ?: throw PinBoardSettingInvalidBoardError()
+        if (board.author != user) throw PinBoardSettingInvalidBoardError()
+        return board
+    }
+}
